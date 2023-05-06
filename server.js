443de07bc82b49aa7f64407531749195b595b514
Brynna Miller <brynnam@email.unc.edu>
2023-05-05 22:22:47 -0400

    finish api setup

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ import minimist from "minimist";
 import express from "express";
 
 const app = express();
+
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+
 const args = minimist(process.argv.slice(2));
 const port = args["port"] || 500;
