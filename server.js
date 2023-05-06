7d25c2702d18c0bcb526363b5e1260250450d7a2
Brynna Miller <brynnam@email.unc.edu>
2023-05-05 22:20:13 -0400

    declaring app and port

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ import { rps, rpsls } from "./lib/rpsls";
 
 import minimist from "minimist";
 import express from "express";
+
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args["port"] || 500;
