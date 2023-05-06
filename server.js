4ce24851273f2e23beec0e714cbe2cea95a7c66b
Brynna Miller <brynnam@email.unc.edu>
2023-05-05 22:27:03 -0400

    C:/Program Files/Git/app/ endpoint -> 200

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.use(express.urlencoded({extended: true}));
 
 const args = minimist(process.argv.slice(2));
 const port = args["port"] || 500;
+
+//check endpoint at /app/ that returns 200 OK
+app.get("/app/", (req, res) =>{
+    res.status(200).send('200 OK');
+})
